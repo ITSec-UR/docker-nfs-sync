f33ea7c278eeafd21f0f6a1b0e698ac7e8d01e9f
Philipp Holler <philipp.holler93@googlemail.com>
2016-08-13 18:35:42 +0200

    added some status updates in console and suppressed umount output

--- nfs-sync_entrypoint.sh
+++ nfs-sync_entrypoint.sh
@@ -18,9 +18,13 @@ fi
 
 : ${LOCAL_MOUNTDIR=/tmp/remote-mount}
 
+echo "Mounting remote directory $NFS_SERVER:$REMOTE_SHARE..."
 mkdir -p $LOCAL_MOUNTDIR
 umount $LOCAL_MOUNTDIR || /bin/true
 mount -t nfs $NFS_SERVER:$REMOTE_SHARE $LOCAL_MOUNTDIR
 
+echo "Syncing files from $NFS_SERVER:$REMOTE_SHARE to $SYNC_DIR..."
 mkdir -p $SYNC_DIR
 rsync -tr $LOCAL_MOUNTDIR/* $SYNC_DIR/
+
+echo "Done!"
