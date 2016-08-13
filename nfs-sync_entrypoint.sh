b45d61309d5088613a8bf40d35df96fbcd9c17ec
Philipp Holler <philipp.holler93@googlemail.com>
2016-08-13 17:55:48 +0200

    initial commit

---
+++ nfs-sync_entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+if [ -z "$NFS_SERVER" ]; then
+  echo "NFS_SERVER not set."
+  exit 1
+fi
+
+if [ -z "$REMOTE_SHARE" ]; then
+  echo "REMOTE_SHARE not set."
+  exit 1
+fi
+
+if [ -z "$SYNC_DIR" ]; then
+  echo "SYNC_DIR not set."
+  exit 1
+fi
+
+: ${LOCAL_MOUNTDIR=/tmp/remote-mount}
+
+mkdir -p $LOCAL_MOUNTDIR
+umount $LOCAL_MOUNTDIR
+mount -t nfs $NFS_SERVER:$REMOTE_SHARE $LOCAL_MOUNTDIR
+
+rsync -tr $LOCAL_MOUNTDIR/* $SYNC_DIR
